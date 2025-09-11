c6280162f54a9a12b98c343083a303b275d30776
Minsu Lee <amond@amond.net>
2025-09-11 09:18:25 +0000

    feat(aws): add installation script for AWS CLI

--- install-root-sdk.sh
+++ install-root-sdk.sh
@@ -1,3 +1,12 @@
 # Qodana Cli 설치
 echo "Installing Qodana CLI..."
 curl -fsSL https://jb.gg/qodana-cli/install | bash
+echo "Qodana CLI installed successfully."
+
+# AWS CLI 설치
+echo "Installing AWS CLI..."
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+rm -rf awscliv2.zip aws
+echo "AWS CLI installed successfully."
